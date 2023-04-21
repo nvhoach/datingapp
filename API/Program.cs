11f608d5584206a74685896e4d52d164134bfc54
Hoach <nvhoach.ktpmk12@gmail.com>
2023-04-21 13:40:06 +0700

    client

--- API/Program.cs
+++ API/Program.cs
@@ -12,10 +12,13 @@ builder.Services.AddDbContext<DataContext>(options =>
         options.UseSqlite(builder.Configuration.GetConnectionString("defaultConnection"));
     });
 
+builder.Services.AddCors();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 app.MapControllers();
 
 app.Run();
