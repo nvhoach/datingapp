c3d2a9a35869ed23fe561eea7326086564f0ea03
Hoach <nvhoach.ktpmk12@gmail.com>
2024-02-03 23:24:48 +0700

    update code

--- API/Program.cs
+++ API/Program.cs
@@ -1,4 +1,6 @@
 using API.Data;
+using API.Properties;
+using API.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -13,6 +15,7 @@ builder.Services.AddDbContext<DataContext>(options =>
     });
 
 builder.Services.AddCors();
+builder.Services.AddScoped<ITokenService, TokenService>();
 
 var app = builder.Build();
 
